68cc62010e346c41f38bfa020cb51bb442a97a5a
Mateusz Labud <mateuszwwe999@interia.pl>
2022-06-04 22:44:43 +0200

    Piąty

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,33 +12,44 @@ class TestController extends AbstractController
     public function index(): Response
     {
 
-        $tablica=[['rcp'=> 19505, 'inicial'=> 'ML', 'ilosc'=> 893],
-        ['rcp'=> 3456, 'inicial'=> 'KL', 'ilosc'=> 870],
-        ['rcp'=> 3456, 'inicial'=> 'FD', 'ilosc'=> 840],
-        ['rcp'=> 35463, 'inicial'=> 'SF', 'ilosc'=> 820],
-        ['rcp'=> 24505, 'inicial'=> 'AS', 'ilosc'=> 800],
-        ['rcp'=> 9505, 'inicial'=> 'FD', 'ilosc'=> 735],
-        ['rcp'=> 4567, 'inicial'=> 'GF', 'ilosc'=> 725],
-        ['rcp'=> 5463, 'inicial'=> 'GH', 'ilosc'=> 720],
-        ['rcp'=> 2456, 'inicial'=> 'FG', 'ilosc'=> 700],
-        ['rcp'=> 35423, 'inicial'=> 'HV', 'ilosc'=> 645],
-        ['rcp'=> 2545, 'inicial'=> 'DF', 'ilosc'=> 620],
-        ['rcp'=> 2534, 'inicial'=> 'VC', 'ilosc'=> 600],
-        ['rcp'=> 23454, 'inicial'=> 'SD', 'ilosc'=> 590],
-        ['rcp'=> 2545, 'inicial'=> 'WE', 'ilosc'=> 550],
-        ['rcp'=> 1876, 'inicial'=> 'RY', 'ilosc'=> 540],
-        ['rcp'=> 7564, 'inicial'=> 'UY', 'ilosc'=> 520],
-        ['rcp'=> 4656, 'inicial'=> 'IU', 'ilosc'=> 500],
-        ['rcp'=> 3450, 'inicial'=> 'TY', 'ilosc'=> 400],
-        ['rcp'=> 3456, 'inicial'=> 'GH', 'ilosc'=> 370],
-        ['rcp'=> 7645, 'inicial'=> 'GH', 'ilosc'=> 260],
-        ['rcp'=> 7655, 'inicial'=> 'MN', 'ilosc'=> 230],
-        ['rcp'=> 4532, 'inicial'=> 'LH', 'ilosc'=> 196],
-    ];
-//        dd($tablica);
+        $tablica=[['rcp'=> 111, 'inicial'=> 'ML', 'ilosc'=> 893],
+        ['rcp'=> 222, 'inicial'=> 'KL', 'ilosc'=> 870],
+        ['rcp'=> 333, 'inicial'=> 'FD', 'ilosc'=> 840],
+        ['rcp'=> 444, 'inicial'=> 'SF', 'ilosc'=> 820],
+        ['rcp'=> 555, 'inicial'=> 'AS', 'ilosc'=> 800],
+        ['rcp'=> 666, 'inicial'=> 'FD', 'ilosc'=> 735],
+        ['rcp'=> 777, 'inicial'=> 'GF', 'ilosc'=> 725],
+        ['rcp'=> 888, 'inicial'=> 'GH', 'ilosc'=> 720],
+        ['rcp'=> 999, 'inicial'=> 'FG', 'ilosc'=> 700],
+        ['rcp'=> 101010, 'inicial'=> 'HV', 'ilosc'=> 645],
+        ['rcp'=> 111111, 'inicial'=> 'DF', 'ilosc'=> 620],
+        ['rcp'=> 121212, 'inicial'=> 'VC', 'ilosc'=> 600],
+        ['rcp'=> 131313, 'inicial'=> 'SD', 'ilosc'=> 590],
+        ['rcp'=> 141414, 'inicial'=> 'WE', 'ilosc'=> 550],
+        ['rcp'=> 151515, 'inicial'=> 'RY', 'ilosc'=> 540],
+        ['rcp'=> 161616, 'inicial'=> 'UY', 'ilosc'=> 520],
+        ['rcp'=> 171717, 'inicial'=> 'IU', 'ilosc'=> 500],
+        ['rcp'=> 181818, 'inicial'=> 'TY', 'ilosc'=> 400],
+        ['rcp'=> 191919, 'inicial'=> 'GH', 'ilosc'=> 370],
+        ['rcp'=> 202020, 'inicial'=> 'GH', 'ilosc'=> 260],
+        ['rcp'=> 212121, 'inicial'=> 'MN', 'ilosc'=> 230],
+        ['rcp'=> 222222, 'inicial'=> 'LH', 'ilosc'=> 196],
+        ];
+        $even=null;
+        $ile = count($tablica);
+        if ($ile%2==0){
+            $countArray = ($ile/2)-1;               //Jeśli liczba jest parzysta
+            $even = $countArray;
+        }
+        else {
+            $countArray = ($ile/2)-0.5;             // Jeśli liczba jest nieparzysta
+        }
+
             return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'countArray' => $countArray,
             'repoMsw' => $tablica,
-        ]);
+            'ile' => $ile,
+            'even' => $even,
+            ]);
     }
 }
